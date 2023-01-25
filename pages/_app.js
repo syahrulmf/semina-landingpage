096a7cf2f65099391c237ce3ee5c03e4d3899038
syahrulmf <syahrulmf@gmail.com>
2023-01-25 14:57:35 +0700

    Setup project structures

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '../styles/globals.css'
+/* eslint-disable @next/next/no-sync-scripts */
+/* eslint-disable @next/next/no-css-tags */
+import "../styles/globals.css";
+import "../styles/main.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer position="top-right" />
+    </>
+  );
 }
+
+export default MyApp;
